17642811e91fae9c833e891f25b7f2ab916f38e3
cameron mcinnes <cam.mcinnes@gmail.com>
2018-03-25 20:33:29 -0400

    Added subway badge animations and ability to dispatch trains on keypress

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,8 @@ module.exports = {
   entry: "./lib/entry.js",
   output: {
     filename: 'bundle.js',
-     path: path.resolve(__dirname, 'lib')
+    path: path.resolve(__dirname, 'lib'),
+    publicPath: '/assets/'
   },
   devtool: 'source-map',
   mode: 'development'
